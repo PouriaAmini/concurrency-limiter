56d91cc6cbe85d2a4726c357d63c3c6feadd4ea7
Pouria Amini <aminipooria1@gmail.com>
2023-09-10 11:20:27 -0400

    Add nginx proxy to intercept client requests and forward them to server

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -19,6 +19,7 @@ type config struct {
 	port       int
 	targetPort int
 	rate       int
+	quota      int
 }
 
 var cfg config
@@ -59,10 +60,12 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 8081, "port to listen on")
 	flag.IntVar(&cfg.targetPort, "targetPort", 8080, "port to send requests to")
 	flag.IntVar(&cfg.rate, "rate", 2, "number of requests to send per second")
+	flag.IntVar(&cfg.quota, "quota", 1, "number of concurrent quota")
 	flag.Parse()
 
 	successRespCh := make(chan bool, 1)
 
+	// Create a client with a custom transport to limit the number of concurrent requests
 	client := &http.Client{
 		Timeout: 3 * time.Second,
 		Transport: &http.Transport{
@@ -79,9 +82,20 @@ func main() {
 		}
 	}()
 
+	// Send request to the server logic
 	var get = func() {
 		start := time.Now()
-		resp, err := client.Get(fmt.Sprintf("http://localhost:%d", cfg.targetPort))
+
+		req, err := http.NewRequest("GET", fmt.Sprintf("http://localhost:%d", cfg.targetPort), nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		req.Header.Set("user-agent", fmt.Sprintf("client%d", cfg.id))
+		req.Header.Set("x-quota", fmt.Sprintf("%d", cfg.quota))
+
+		resp, err := client.Do(req)
 		if err != nil {
 			var errTimeout *url.Error
 			if errors.As(err, &errTimeout) && errTimeout.Timeout() {
@@ -90,6 +104,7 @@ func main() {
 			fmt.Println(err)
 			return
 		}
+
 		defer func() {
 			err := resp.Body.Close()
 			if err != nil {
@@ -98,14 +113,17 @@ func main() {
 			}
 			successRespCh <- true
 		}()
+
 		_, err = io.Copy(os.Stdout, resp.Body)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+
 		fmt.Printf("Client %d: %s\n", cfg.id, time.Since(start))
 	}
 
+	// Keep track of the throughput
 	go func() {
 		respHistoryQueue := make([]bool, 0, cfg.rate)
 		for {
@@ -122,6 +140,7 @@ func main() {
 		}
 	}()
 
+	// Send requests at a fixed rate
 	ticker := time.NewTicker(time.Second / time.Duration(cfg.rate))
 	defer ticker.Stop() // Not triggered since the program is killed with Ctrl-C
 	for {
